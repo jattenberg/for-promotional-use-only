5534352b3b184c5cbfc40268aed63cc817557c64
Josh Attenberg <josh.attenberg@gmail.com>
2020-10-29 20:57:15 -0400

    black

--- for-promotional-use-only/generate_json.py
+++ for-promotional-use-only/generate_json.py
@@ -7,9 +7,10 @@ import orjson
 
 BUCKET = "for-promotional-use-only.com"
 FOLDER = "mixtape/"
-s3_client = boto3.client('s3')
+s3_client = boto3.client("s3")
 OUT_DIR = "build/json/"
 LETTERS = string.ascii_uppercase
+
 
 def get_all_s3_objects(s3, **base_kwargs):
     """
@@ -20,38 +21,39 @@ def get_all_s3_objects(s3, **base_kwargs):
     while True:
         list_kwargs = dict(MaxKeys=1000, **base_kwargs)
         if continuation_token:
-            list_kwargs['ContinuationToken'] = continuation_token
+            list_kwargs["ContinuationToken"] = continuation_token
         response = s3.list_objects_v2(**list_kwargs)
-        yield from response.get('Contents', [])
-        if not response.get('IsTruncated'):  # At the end of the list?
+        yield from response.get("Contents", [])
+        if not response.get("IsTruncated"):  # At the end of the list?
             break
-        continuation_token = response.get('NextContinuationToken')
+        continuation_token = response.get("NextContinuationToken")
 
-def song_iterator(bucket=BUCKET,
-                  folder=FOLDER):
+
+def song_iterator(bucket=BUCKET, folder=FOLDER):
 
     for file in get_all_s3_objects(s3_client, Bucket=bucket, Prefix=folder):
-        if file['Key'] != folder:
+        if file["Key"] != folder:
             yield file
+
 
 def pretty_print_json(path, filename, data):
     full_path = os.path.join(path, filename)
     logging.info("writing to %s" % full_path)
-    with open(full_path, 'wb') as f:
-        f.write(orjson.dumps(
-            data,
-            option=orjson.OPT_INDENT_2
-        ))
+    with open(full_path, "wb") as f:
+        f.write(orjson.dumps(data, option=orjson.OPT_INDENT_2))
+
 
 def main():
-    logging.basicConfig(format='%(asctime)s %(message)s', stream=sys.stdout, level="INFO")
+    logging.basicConfig(
+        format="%(asctime)s %(message)s", stream=sys.stdout, level="INFO"
+    )
 
-    lists = {'NUM': []}
+    lists = {"NUM": []}
     for letter in LETTERS:
         lists[letter] = []
 
     for song in song_iterator():
-        name = song['Key']
+        name = song["Key"]
         if not (name.endswith("mp4") or name.endswith("m4a")):
             pass
 
@@ -62,7 +64,6 @@ def main():
 
     for key, value in lists.items():
         pretty_print_json(OUT_DIR, "%ssongs.json" % key, value)
-
 
 
 if __name__ == "__main__":
